3e0faffec397c0ef0bb15b921bd297072f0427ae
joseluisgs <joseluisgonsan@gmail.com>
2020-12-01 11:15:11 +0100

    correcciones

--- ejemplos/ejem07/code/myapp/index.php
+++ ejemplos/ejem07/code/myapp/index.php
@@ -4,13 +4,14 @@ echo ("<h3>He montado LEMP</h3>");
 
 try {
   // Importante, la dirección del host es como hayamos en el docker, en mi caso es mariadb
+  $servertype = "mysql";
   $servername = "mariadb";
   $username = "root";
   $password = "password";
   $database = "docker_sample";
   $port = "3306";
 
-  $pdo = new PDO("mysql:host=$servername; port=$port; dbname=$database", $username, $password);
+  $pdo = new PDO("$servertype:host=$servername; port=$port; dbname=$database", $username, $password);
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Conexión Realizada con éxito";
 
